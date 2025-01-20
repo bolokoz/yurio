8b104288f7f602d4a8492a27c546d47cdf85fe34
bolokoz <yurifbeck@gmail.com>
2025-01-19 20:03:04 -0400

    menu with content

--- layouts/AppMenu.vue
+++ layouts/AppMenu.vue
@@ -1,190 +1,257 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-import AppMenuItem from './AppMenuItem.vue';
+import AppMenuItem from "./AppMenuItem.vue";
+import { PanelMenu } from "primevue";
+
+const { data: contentNavigation } = await useAsyncData("navigation", () => {
+  return queryCollectionNavigation("content");
+});
+const menuItens = computed(() => {
+  console.log("value", contentNavigation.value[0]);
+  // Ensure data is available and has the expected structure
+  if (
+    !contentNavigation.value[0] ||
+    !Array.isArray(contentNavigation.value[0].children)
+  )
+    return null;
+
+
+  let children = contentNavigation.value[0].children.map((item) => ({
+    label: item.title,
+    to: item.path, // Use a fallback to avoid accessing undefined
+  }))
+
+  return [{
+    label: contentNavigation.value[0].title,
+    items: children,
+  }];
+});
+
+// const transformedData = data[0].map(({title, children}) => ({
+//     label: title,
+//     itens: children
+// }))
 
 const model = ref([
-    {
-        label: 'Home',
+  {
+    label: "Home",
+    items: [
+      { label: "Notes", icon: "pi pi-fw pi-book", to: "/notes" },
+      {
+        label: "Parmegianologo",
+        icon: "pi pi-fw pi-heart-fill",
+        to: "/parmegianologo",
+      },
+      { label: "Demo Dashboard", icon: "pi pi-fw pi-home", to: "/" },
+    ],
+  },
+  {
+    label: "Mini apps",
+    items: [
+      {
+        label: "Parmegianologo",
+        icon: "pi pi-fw pi-food",
+        to: "/parmegianologo",
+      },
+      {
+        label: "Engenharia",
+        icon: "pi pi-fw pi-user",
         items: [
-            { label: 'Notes', icon: 'pi pi-fw pi-book', to: '/notes' },
-            { label: 'Parmegianologo', icon: 'pi pi-fw pi-heart-fill', to: '/parmegianologo' },
-            { label: 'Demo Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
-
-        ]
-    },
-    {
-        label: 'Mini apps',
+          {
+            label: "Calculadora",
+            icon: "pi pi-fw pi-sign-in",
+            to: "/eng/calculadora",
+          },
+          {
+            label: "teste",
+            icon: "pi pi-fw pi-sign-in",
+            to: "/eng/test",
+          },
+          {
+            label: "Calculadora",
+            icon: "pi pi-fw pi-sign-in",
+            to: "/eng/estimativa_koeroga",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "Projects",
+    items: [
+      {
+        label: "Form Layout",
+        icon: "pi pi-fw pi-id-card",
+        to: "/uikit/formlayout",
+      },
+      { label: "Input", icon: "pi pi-fw pi-check-square", to: "/uikit/input" },
+      {
+        label: "Button",
+        icon: "pi pi-fw pi-mobile",
+        to: "/uikit/button",
+        class: "rotated-icon",
+      },
+      { label: "Table", icon: "pi pi-fw pi-table", to: "/uikit/table" },
+      { label: "List", icon: "pi pi-fw pi-list", to: "/uikit/list" },
+      { label: "Tree", icon: "pi pi-fw pi-share-alt", to: "/uikit/tree" },
+      { label: "Panel", icon: "pi pi-fw pi-tablet", to: "/uikit/panel" },
+      { label: "Overlay", icon: "pi pi-fw pi-clone", to: "/uikit/overlay" },
+      { label: "Media", icon: "pi pi-fw pi-image", to: "/uikit/media" },
+      { label: "Menu", icon: "pi pi-fw pi-bars", to: "/uikit/menu" },
+      { label: "Message", icon: "pi pi-fw pi-comment", to: "/uikit/message" },
+      { label: "File", icon: "pi pi-fw pi-file", to: "/uikit/file" },
+      { label: "Chart", icon: "pi pi-fw pi-chart-bar", to: "/uikit/charts" },
+      {
+        label: "Timeline",
+        icon: "pi pi-fw pi-calendar",
+        to: "/uikit/timeline",
+      },
+      { label: "Misc", icon: "pi pi-fw pi-circle", to: "/uikit/misc" },
+    ],
+  },
+  {
+    label: "Pages",
+    icon: "pi pi-fw pi-briefcase",
+    to: "/pages",
+    items: [
+      {
+        label: "Landing",
+        icon: "pi pi-fw pi-globe",
+        to: "/landing",
+      },
+      {
+        label: "Notes",
+        icon: "pi pi-fw pi-globe",
+        to: "/notes/eng/test",
+      },
+      {
+        label: "Auth",
+        icon: "pi pi-fw pi-user",
         items: [
-        { label: 'Parmegianologo', icon: 'pi pi-fw pi-food', to: '/parmegianologo' },
-        {
-                label: 'Engenharia',
-                icon: 'pi pi-fw pi-user',
-                items: [
-                    {
-                        label: 'Calculadora',
-                        icon: 'pi pi-fw pi-sign-in',
-                        to: '/eng/calculadora'
-                    },
-                    {
-                        label: 'teste',
-                        icon: 'pi pi-fw pi-sign-in',
-                        to: '/eng/test'
-                    },
-                    {
-                        label: 'Calculadora',
-                        icon: 'pi pi-fw pi-sign-in',
-                        to: '/eng/estimativa_koeroga'
-                    }
-                ]
-            },
-        ]
-    },
-    {
-        label: 'Projects',
+          {
+            label: "Login",
+            icon: "pi pi-fw pi-sign-in",
+            to: "/auth/login",
+          },
+          {
+            label: "Error",
+            icon: "pi pi-fw pi-times-circle",
+            to: "/auth/error",
+          },
+          {
+            label: "Access Denied",
+            icon: "pi pi-fw pi-lock",
+            to: "/auth/access",
+          },
+        ],
+      },
+      {
+        label: "Crud",
+        icon: "pi pi-fw pi-pencil",
+        to: "/crud",
+      },
+      {
+        label: "Not Found",
+        icon: "pi pi-fw pi-exclamation-circle",
+        to: "/notfound",
+      },
+      {
+        label: "Empty",
+        icon: "pi pi-fw pi-circle-off",
+        to: "/empty",
+      },
+    ],
+  },
+  {
+    label: "Hierarchy",
+    items: [
+      {
+        label: "Submenu 1",
+        icon: "pi pi-fw pi-bookmark",
         items: [
-            
-
-            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
-            { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
-            { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
-            { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
-            { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-            { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-            { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
-            { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-            { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
-            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu' },
-            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
-            { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
-            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
-            { label: 'Timeline', icon: 'pi pi-fw pi-calendar', to: '/uikit/timeline' },
-            { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
-        ]
-    },
-    {
-        label: 'Pages',
-        icon: 'pi pi-fw pi-briefcase',
-        to: '/pages',
+          {
+            label: "Submenu 1.1",
+            icon: "pi pi-fw pi-bookmark",
+            items: [
+              { label: "Submenu 1.1.1", icon: "pi pi-fw pi-bookmark" },
+              { label: "Submenu 1.1.2", icon: "pi pi-fw pi-bookmark" },
+              { label: "Submenu 1.1.3", icon: "pi pi-fw pi-bookmark" },
+            ],
+          },
+          {
+            label: "Submenu 1.2",
+            icon: "pi pi-fw pi-bookmark",
+            items: [{ label: "Submenu 1.2.1", icon: "pi pi-fw pi-bookmark" }],
+          },
+        ],
+      },
+      {
+        label: "Submenu 2",
+        icon: "pi pi-fw pi-bookmark",
         items: [
-            {
-                label: 'Landing',
-                icon: 'pi pi-fw pi-globe',
-                to: '/landing'
-            },
-            {
-                label: 'Notes',
-                icon: 'pi pi-fw pi-globe',
-                to: '/notes/eng/test'
-            },
-            {
-                label: 'Auth',
-                icon: 'pi pi-fw pi-user',
-                items: [
-                    {
-                        label: 'Login',
-                        icon: 'pi pi-fw pi-sign-in',
-                        to: '/auth/login'
-                    },
-                    {
-                        label: 'Error',
-                        icon: 'pi pi-fw pi-times-circle',
-                        to: '/auth/error'
-                    },
-                    {
-                        label: 'Access Denied',
-                        icon: 'pi pi-fw pi-lock',
-                        to: '/auth/access'
-                    }
-                ]
-            },
-            {
-                label: 'Crud',
-                icon: 'pi pi-fw pi-pencil',
-                to: '/crud'
-            },
-            {
-                label: 'Not Found',
-                icon: 'pi pi-fw pi-exclamation-circle',
-                to: '/notfound'
-            },
-            {
-                label: 'Empty',
-                icon: 'pi pi-fw pi-circle-off',
-                to: '/empty'
-            }
-        ]
-    },
-    {
-        label: 'Hierarchy',
-        items: [
-            {
-                label: 'Submenu 1',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 1.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 1.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                    }
-                ]
-            },
-            {
-                label: 'Submenu 2',
-                icon: 'pi pi-fw pi-bookmark',
-                items: [
-                    {
-                        label: 'Submenu 2.1',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [
-                            { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                            { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                        ]
-                    },
-                    {
-                        label: 'Submenu 2.2',
-                        icon: 'pi pi-fw pi-bookmark',
-                        items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        label: 'Get Started',
-        items: [
-            {
-                label: 'Documentation',
-                icon: 'pi pi-fw pi-book',
-                to: '/documentation'
-            },
-            {
-                label: 'View Source',
-                icon: 'pi pi-fw pi-github',
-                url: 'https://github.com/suprimpoudel/sakai-nuxt',
-                target: '_blank'
-            }
-        ]
-    }
+          {
+            label: "Submenu 2.1",
+            icon: "pi pi-fw pi-bookmark",
+            items: [
+              { label: "Submenu 2.1.1", icon: "pi pi-fw pi-bookmark" },
+              { label: "Submenu 2.1.2", icon: "pi pi-fw pi-bookmark" },
+            ],
+          },
+          {
+            label: "Submenu 2.2",
+            icon: "pi pi-fw pi-bookmark",
+            items: [{ label: "Submenu 2.2.1", icon: "pi pi-fw pi-bookmark" }],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: "Get Started",
+    items: [
+      {
+        label: "Documentation",
+        icon: "pi pi-fw pi-book",
+        to: "/documentation",
+      },
+      {
+        label: "View Source",
+        icon: "pi pi-fw pi-github",
+        url: "https://github.com/suprimpoudel/sakai-nuxt",
+        target: "_blank",
+      },
+    ],
+  },
 ]);
 </script>
 
 <template>
-    <ul class="layout-menu">
-        <template v-for="(item, i) in model" :key="item">
-            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
-            <li v-if="item.separator" class="menu-separator"></li>
-        </template>
-    </ul>
+  <ul class="layout-menu">
+    <!-- <PanelMenu :model="menuItens" class="w-full md:w-20rem">
+        <template #item="{ item }">
+                <NuxtLink :to="item.to">
+                    <span class="ml-2 text-color">{{ item.label }}</span>
+                </NuxtLink>
+            </template>
+    </PanelMenu> -->
+    <template v-for="(item, i) in model" :key="item">
+      <app-menu-item
+        v-if="!item.separator"
+        :item="item"
+        :index="i"
+      ></app-menu-item>
+      <li v-if="item.separator" class="menu-separator"></li>
+    </template>
+    <template v-for="(item, i) in menuItens" :key="item">
+      <app-menu-item
+        v-if="!item.separator"
+        :item="item"
+        :index="i"
+      ></app-menu-item>
+      <li v-if="item.separator" class="menu-separator"></li>
+    </template>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
--- pages/notes/[...slug].vue
+++ pages/notes/[...slug].vue
@@ -3,8 +3,15 @@ const route = useRoute()
 const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection('content').path(route.path).first()
 })
+
+
 </script>
 
 <template>
   <ContentRenderer v-if="page" :value="page" />
+  <div v-else>
+    <div class="min-h-screen flex items-center justify-center bg-gray-200">
+      Not found
+    </div>
+  </div>
 </template>
