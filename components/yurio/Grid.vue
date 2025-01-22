916c45f2570f5e048512dbdbb01bf26cae206d4c
bolokoz <yurifbeck@gmail.com>
2025-01-22 02:53:53 -0300

    added title components

---
+++ components/yurio/Grid.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg-surface-0 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20 text-center">
+        <div class="mb-4 font-bold text-3xl">
+            <span class="text-surface-900 capitalize dark:text-surface-0">{{ route.path.split('/')[1] }}: </span>
+            <span class="text-primary-600  capitalize dark:text-primary-400">{{ route.path.split('/')[2] }}</span>
+        </div>
+        <!-- <div class="text-surface-700 dark:text-surface-0/70 mb-[3rem]">Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna.</div> -->
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-3.5 gap-y-6">
+            <div v-for="item in props" class="w-full p-4">
+                <div class="text-surface-900 dark:text-surface-0 text-xl mb-2 font-medium">{{ item[0].title }}</div>
+                <p class="text-surface-700 dark:text-surface-0/70 leading-normal max-w-sm mx-auto text-center">{{ item[0].title }}</p>
+            </div>
+            
+        </div>
+    </div>
+</template>
+<script setup>
+
+const route = useRoute()
+
+const props = defineProps(['items'])
+
+</script>
---
+++ components/yurio/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-surface-0 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20">
+        <div class="text-surface-700 dark:text-surface-100 text-center">
+            <div class="text-primary font-bold mb-4"><i class="pi pi-info" />&nbsp;{{ subtitle }}</div>
+            <div class="text-surface-900 capitalize dark:text-surface-0 font-bold text-5xl mb-4">{{title}}</div>
+            <!-- <div class="text-surface-700 dark:text-surface-100 text-2xl mb-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit numquam eligendi quos.</div> -->
+            <NuxtLink :to="`https://github.com/bolokoz/yurio/tree/main/${route.fullPath}`"  target="_blank" rel="noopener">
+            <Button label="Edit in Github" icon="pi pi-github" raised rounded class="font-bold px-8 py-4 whitespace-nowrap" />
+            </NuxtLink> 
+        </div>
+    </div>
+</template>
+<!-- :to=`github.com/bolokoz/yurio/${path}` -->
+<script setup lang="ts">
+const route = useRoute()
+const { title = 'Categoria', subtitle } = defineProps<{title?:string,subtitle?:string}>()
+</script>
